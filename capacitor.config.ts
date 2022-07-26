e1b478e1d6f579161797e5d75e5680e048bd7979
SHOTORAK\Saeid <16025625+saostad@users.noreply.github.com>
2022-07-26 16:38:18 -0400

    - capacitor added

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.us_citizenship_interview.app',
+  appName: 'us-citizenship-interview',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
