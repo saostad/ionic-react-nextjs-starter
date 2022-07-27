8ef8294c7c1a6ebe7880f79554eeccd635b3e78b
SHOTORAK\Saeid <16025625+saostad@users.noreply.github.com>
2022-07-26 20:05:15 -0400

    middle: capacitor setup

---
+++ android/app/src/main/java/com/us_citizenship_interview/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.us_citizenship_interview.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.us_citizenship_interview.app',
-  appName: 'us-citizenship-interview',
-  webDir: 'public',
-  bundledWebRuntime: false
+  appId: "com.us_citizenship_interview.app",
+  appName: "us-citizenship-interview",
+  webDir: "out",
+  bundledWebRuntime: false,
 };
 
 export default config;
